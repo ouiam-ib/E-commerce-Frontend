f7bede85a91c84b666c7a5d02d98083b6d0cc3be
ouiam-ib <itsouiamibrahimi@gmail.com>
2024-04-26 14:56:52 +0100

    List of products

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,11 +1,12 @@
 export class Product {
+    // the proprities should be the exact same as the ones in Product in spring boot
     constructor(public id:number,
         public name:string,
         public description: string,
         public unitPrice: number,
-        public imageURL:string,
+        public imageUrl:string,
         public active: boolean,
-        public unitInStock:number,
+        public unitsInStock:number,
         public datecreated : Date,
         public lastUpdate: Date
         ){}
