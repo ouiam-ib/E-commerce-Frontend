8188dbce59f92f749a1d5b3beea96eced965453b
ouiam-ib <itsouiamibrahimi@gmail.com>
2024-05-09 16:26:21 +0100

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+
 @NgModule({
   declarations: [
     AppComponent,
